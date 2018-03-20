3f8a494b14053eb1abdd12c2ceadbc69645cf98c
Rafi Islam <rafi.islam@statravel.com>
2018-03-20 15:08:42 +0100

    adding split-urls.py

---
+++ split-urls.py
@@ -0,0 +1,88 @@
+import sys
+import requests
+import math
+from datetime import datetime
+
+startTime = datetime.now()
+#read 2 params
+if len(sys.argv) < 3:
+  print("Please pass the project folder as param1 and redirect file as param2")
+  print("i.e. python split.py uk_division_web_live get-redirects-uk-en")
+  quit()
+else:
+  project = sys.argv[1] 
+  redirect = sys.argv[2]
+
+  # Get the urls from the API
+  api = "http://s607650rgvl190.bluee.net:8080/cps/rde/rest/v1/project/"+project+"/contents?group=content&type=HTML&sortorder=asc&sortedby=changed&chunksize=250&chunk="
+  print ("Start crawling URL's from "+ api +" ...")
+  response = requests.get(api)
+  data = response.json()
+  num_of_pages = int(data['hits'])
+  iteration_needed = math.ceil(num_of_pages/250)
+
+  pages = set()
+  for p in range(1, iteration_needed +1):
+    contents = requests.get(api+str(p)).json()['contents']
+    #loop through the contents array and find out only name with html extension
+    contents_length = len(contents)
+    for x in range(0, contents_length):
+
+      if contents[x]['name'][-4:] == '.htm':
+       pages.add(contents[x]['name'])
+  
+  # now we have all the pages with .htm in DS
+  # lets check the files in the redirect file 
+  print ("Done with crawling ...")
+  print ("Opening the file "+ redirect +" ...")
+  lines = set()
+  with open(redirect) as f:
+    for line in f:
+      # strip new lines and split on tab - get the first value, right strip /-
+      # debug : there are some strings separated by space - split them as well
+      # debug : get rid of params
+      # split on / and get the last value - it should be the page name
+      line = line.strip().split("\t")[0].split(" ")[0].rstrip('/-').rstrip('\-').split('/')[-1].split("?")[0].split("#")[0]
+      lines.add(line)
+  print ("Done with file reading ...")
+  print ("Start processing ...")
+  # filter out empty values and get the unique URL's
+  lines = set(filter(None, lines))
+  redirect_in_zxtm = set()
+  redirect_in_cms = set()
+  delete_from_project = set()
+  # now loop throug lines and check if it exists in the project
+  # url without .htm -> redirect using ZXTM
+  # url exist in the project -> redirect using CMS
+  # url doesn't exist in the project -> delete them 
+
+  for path in lines:
+    if path[-4:] != '.htm':
+      redirect_in_zxtm.add(path)
+    elif path in pages:
+      redirect_in_cms.add(path)
+    else:
+      delete_from_project.add(path)
+ 
+  redirect_in_zxtm_headline = '*'*72 + "\n Can be redirected in ZXTM \n" + " Total Pages : " + str(len(redirect_in_zxtm)) + "\n" +  '*'*72 + "\n"
+  redirect_in_cms_headline = '*'*72 + "\n Can be redirected in CMS \n" + " Total Pages : " + str(len(redirect_in_cms)) + "\n" +  '*'*72 + "\n"
+  delete_from_project_headline = '*'*72 + "\n Can be deleted in CMS \n" + " Total Pages : " + str(len(delete_from_project)) + "\n" +  '*'*72 + "\n"
+   #wtite the files
+  with open('redirect_in_zxtm.txt', 'w') as f:
+    f.write(redirect_in_zxtm_headline)
+    f.write('\n'.join(redirect_in_zxtm))
+  with open('redirect_in_cms.txt', 'w') as f:
+    f.write(redirect_in_cms_headline)
+    f.write('\n'.join(redirect_in_cms))
+  with open('delete_from_project.txt', 'w') as f:
+    f.write(delete_from_project_headline)
+    f.write('\n'.join(delete_from_project))
+  #print to console
+  print(redirect_in_zxtm_headline)
+  print (redirect_in_zxtm)
+  print(redirect_in_cms_headline)
+  print (redirect_in_cms)
+  print(delete_from_project_headline)
+  print (delete_from_project)
+
+  print("Time taken in  hh:mm:ss => " + str(datetime.now() - startTime))
